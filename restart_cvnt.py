84ac9a3044d547fd6fca8c834ce332e8e02ad2e2
zhaoshuaiji <45218760+lsy-zhaoshuaiji@users.noreply.github.com>
2019-09-17 18:58:47 +0800

    Add files via upload

--- restart_cvnt.py
+++ restart_cvnt.py
@@ -3,5 +3,6 @@ status=subprocess.getoutput('docker start $(docker ps -aq)')
 output=subprocess.getoutput('docker ps -q')
 container_list=output.split('\n')
 for docker_name in container_list:
-    shell='docker exec %s bash -c "cd /usr/local/yyets_20190829/yyets_20190829;./install.sh start;"'%docker_name
-    print(shell)
+    shell='docker exec %s bash -c "cd /usr/local/yyets_20190829/yyets_20190829;./install.sh stop;./install.sh start;"'%docker_name
+    output=subprocess.getoutput(shell)
+    print(output)
