7ffd14af45e53a4018b81f323e9f06f49172f92e
chinaqiao <chinaqiaoyue@gmail.com>
2019-09-18 21:24:56 +0800

    重启时，增加数量提示进度，和启动的容器名

--- restart_cvnt.py
+++ restart_cvnt.py
@@ -2,7 +2,11 @@ import subprocess
 status=subprocess.getoutput('docker start $(docker ps -aq)')
 output=subprocess.getoutput('docker ps -q')
 container_list=output.split('\n')
+name_num = 1
 for docker_name in container_list:
+    print(docker_name)
+    print(name_num)
     shell='docker exec %s bash -c "cd /usr/local/yyets_20190829/yyets_20190829;./install.sh stop;./install.sh start;"'%docker_name
     output=subprocess.getoutput(shell)
     print(output)
+    name_num += 1
